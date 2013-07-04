adb2fbdff2dad1d15fe8337471742ad25018a99d
Esysteme <aurelien.lequoy@gmail.com>
2013-07-04 12:05:59 +0200

    Create install_mercurial_on_centos_with_php'.sh

---
+++ install_mercurial_on_centos_with_php'.sh
@@ -0,0 +1,28 @@
+# install mercurial on old VM for PHP4
+
+wget http://docutils.sourceforge.net/docutils-snapshot.tgz
+tar -xvf docutils-snapshot.tgz
+cd docutils
+python setup.py install
+
+yum install ruby
+
+
+cd /tmp
+wget http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.noarch.rpm
+rpm -Uvh ec2-ami-tools.noarch.rpm
+
+
+yum install python-devel
+
+
+#say no to installing software else you will install PHP 5
+yum update
+
+
+wget http://mercurial.selenic.com/release/mercurial-1.8.tar.gz
+tar xvzf mercurial-1.8.tar.gz
+cd mercurial-1.8
+make install
+
+#Python headers are required to build Mercurial
